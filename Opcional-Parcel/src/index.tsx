14dfa56c72a52b028a73bed7d6c321e130d6cd14
Sara Casasola <scasasolaborrego@sequel.com>
2020-06-19 21:26:20 +0100

    Opcional - Parcel

---
+++ Opcional-Parcel/src/index.tsx
@@ -0,0 +1,17 @@
+/// <reference path='./index.d.ts'/>
+
+import React from "react";
+import ReactDOM from "react-dom";
+import LogoImg from "./assets/logo.png";
+import "./index.scss";
+
+ReactDOM.render(
+  <div>
+    <header className="imagen">
+        <img src={LogoImg}/>
+    </header>    
+    <p className="texto">Hola mundo</p>    
+    <p className="entorno">Estamos en el entorno de {process.env.ENVIRONMENT}</p>
+  </div>,
+  document.getElementById("root")
+);
